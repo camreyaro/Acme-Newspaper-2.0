10683246eaf3894f60a9ded8ddafe7feeabdb1ca
agepere <36485821+agepere@users.noreply.github.com>
2018-05-03 11:28:53 +0200

    SuscriptionVol corregido

--- Acme-Newspaper/src/main/java/repositories/SuscriptionVolumenRepository.java
+++ Acme-Newspaper/src/main/java/repositories/SuscriptionVolumenRepository.java
@@ -1,6 +1,8 @@
 
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,8 +15,8 @@ public interface SuscriptionVolumenRepository extends JpaRepository<SuscriptionV
 	@Query("select sv from SuscriptionVolumen sv where sv.volumen.id=?1 and sv.customer.id=?2")
 	SuscriptionVolumen getSVFromVolumenAndCustomer(Integer volumenId, Integer customerId);
 
-	@Query("select sv from SuscriptionVolumen sv join sv.volumen v join v.newspapers ns where sv.customer.id=?1 AND (select n from Newspaper n where n.id=?2) in ns")
-	SuscriptionVolumen getSVFromNewspaperAndCustomer(Integer customerId, Integer newspaperId);
+	@Query("select sv from SuscriptionVolumen sv where sv.customer.id=?1 AND (select n from Newspaper n where n.id=?2) member of sv.volumen.newspapers")
+	Collection<SuscriptionVolumen> getSVFromNewspaperAndCustomer(Integer customerId, Integer newspaperId);
 
 	@Query("select 1.0*(count(sv)/((select count(s) from Suscription s) + count(sv))) from SuscriptionVolumen sv")
 	Double ratioSuscriptionVolumen();
